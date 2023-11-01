f0ea73c4ccc4867c38433127064e714ccec72c99
nada <nada50tarek@gmail.com>
2023-11-01 15:51:24 +0200

    set the orientations to portrait on void main

--- graduation_project/lib/main.dart
+++ graduation_project/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../screen/signin_screen.dart';
 
 void main() {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) => runApp(const MyApp()));
+
   runApp(const MyApp());
 }
 
