25b59f0adb02767e883e545fb3a7c8072b67cda1
NadaAshrafMuhammad <monstergirl4480@gmail.com>
2023-11-03 03:28:15 +0200

    adding firebase

--- graduation_project/lib/main.dart
+++ graduation_project/lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../screen/signin_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async{
   
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
   ]).then((_) => runApp(const MyApp()));
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    
+  );
 
   runApp(const MyApp());
 }
