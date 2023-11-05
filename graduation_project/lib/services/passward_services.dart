5c4d93d2014ed6de5cdc4390386867b9444ab7ac
nada <nada50tarek@gmail.com>
2023-11-05 18:14:21 +0200

    create password services

---
+++ graduation_project/lib/services/passward_services.dart
@@ -0,0 +1,55 @@
+import '../helper/api.dart';
+
+class PasswordServices {
+  Future<String> forgetPassword(String email) async {
+    var data = await Api().post(
+      url: 'https://fitsync.onrender.com/api/user/forgetPassword',
+      body: {"email": email},
+      headers: {"Content-Type": "application/json"},
+    );
+
+    // Will return string message of success
+    return data['message'];
+  }
+
+  Future<String> resetPassword(
+      String password, String passwordConf, String token) async {
+    var data = await Api().post(
+      url: 'https://fitsync.onrender.com/api/user/resetPassword',
+      body: {
+        "password": password,
+        "passwordConfirm": passwordConf,
+      },
+      headers: {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      },
+    );
+
+    // Will return token if it not null else will return message
+    return data['token'] ?? data['message'];
+  }
+
+  Future<String> updatePassword({
+    required String oldPassword,
+    required String password,
+    required String passwordConf,
+    required String token,
+  }) async {
+    var data = await Api().patch(
+      url: 'https://fitsync.onrender.com/api/user/resetPassword',
+      body: {
+        "oldPassword": oldPassword,
+        "password": password,
+        "passwordConfirm": passwordConf,
+      },
+      headers: {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      },
+    );
+
+    // Will return token if it not null else will return message
+    return data['token'] ?? data['message'];
+  }
+}
